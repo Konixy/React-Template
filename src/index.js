e31524bf05b1d7db47a0bb47f5a17104a812d51d
Konixy <konixy.p@gmail.com>
2022-09-22 16:33:48 +0200

    Adding tailwind css

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import Player from "./Player";
+import Header from "./Header";
 
 class App extends React.Component {
   render() {
     return (
-    <div>My App</div>
+      <>
+        <header><Header /></header>
+        <div className="player"><Player /></div>
+      </>
     )
   }
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
+// "eslintConfig": {
+//   "extends": [
+//     "react-app",
+//     "react-app/jest"
+//   ]
+// },
