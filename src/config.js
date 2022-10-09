819a297da5d09b5d4997d3809d443fc31496d2bb
Konixy <66780340+Konixy@users.noreply.github.com>
2022-10-09 18:32:09 +0200

    Delete config.js

--- src/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    backendPath: "http://localhost:1000",
-    wsUri: "ws://localhost:8080",
-    spotifyClientId: "6dbc5ffd458d41c1b33a227827d28dea",
-    spotifyClientSecret: "5d2e4364df124131b348069cfb6f5d30",
-    spotifyToken: "BQCjYPayohxF-9rhcffHzr-eJ5pa-I2GcWq8Uj6wEDzvmL99Q1shkB9FkNyHVohdqzU1BdMa0lTk_7bh0RkIa4o5ADq4m8IJgnLv9oxsMF4Xfd8xMTS4xl3UJkdtZApfnzzWGWrw9iWpvVoCHxN-GaHBGzaFKYaF0pJbRXg6MEn5f6MCYy1iRPRGba8nceHx7gc",
-}
