9d60f0d110e907f97baaebf0a2994348d2af553c
Konixy <konixy.p@gmail.com>
2022-12-01 14:05:33 +0100

    Converting project to ViteJs

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export default {
+    backendPath: "http://localhost:1000",
+    wsUri: "ws://localhost:8080",
+    spotifyClientId: "",
+    spotifyClientSecret: "",
+    spotifyToken: "",
+    discordInvite: "",
+    localDevServerId: ""
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./index.scss";
+import "./styles/index.scss";
 import Player from "./Player";
 import Header from "./Header";
 import Home from "./Home";
